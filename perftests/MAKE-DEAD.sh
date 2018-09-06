e4cea55acdd926fa17304259e120ab746692a626
James Craig Burley <james-github@burleyarch.com>
2018-09-06 17:38:00 -0400

    Helper script to generate dead-code versions of form files

---
+++ perftests/MAKE-DEAD.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo >&2 "Usage: $0 <infile> <outfile>"
+    exit 99
+fi
+
+if [ -e $2 ]
+then
+    echo >&2 "$0: ERROR: output file \`$2' already exists."
+    exit 98
+fi
+
+if [ ! -r $1 ]
+then
+    echo >&2 "$0: ERROR: cannot read input file \`$1'."
+    exit 97
+fi
+
+cat > $2 <<EOF
+(cond nil (quote (
+EOF
+
+cat $1 - >> $2 <<EOF
+)))
+EOF
